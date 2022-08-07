921d6d189177f355de6b336cafcc88e591c74fd1
HiBixby <fpssj@naver.com>
2022-08-08 05:10:11 +0900

    🐛 검색기능 오류 해결 (#search 를 .search-bar 로 변경)

--- popup/app.js
+++ popup/app.js
@@ -1,8 +1,10 @@
-const searchEngineUrl =
-  "https://www.smu.ac.kr/search/search.do?menu=통합검색&qt=";
+const searchEngineUrl = 'https://www.smu.ac.kr/search/search.do?menu=통합검색&qt=';
 
-const searchInput = document.querySelector("#search input");
-const searchButton = document.querySelector("#search button");
+const searchInput = document.querySelector('.search-bar input');
+const searchButton = document.querySelector('.search-bar button');
+const searchIcon = document.querySelector('.search-bar i');
+const iconContainer = document.querySelectorAll('.icon-container');
+const iconInButton = document.querySelectorAll('.icon-container i');
 
 function onSearchBtnClick() {
   const searchWord = searchInput.value;
@@ -12,4 +14,9 @@ function onSearchBtnClick() {
     });
   }
 }
-searchButton.addEventListener("click", onSearchBtnClick);
+searchButton.addEventListener('click', onSearchBtnClick);
+
+function onIconContainerHover() {
+  //TODO
+}
+iconContainer.addEventListener('hover', onIconContainerHover);
